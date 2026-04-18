a51066cbc474c9032a3abdb450c3397011bd0c1d
FENIXeury <fenixrenacer0507@gmail.com>
2026-04-18 16:16:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hola Mundo");
+const http = require("http");
+
+const host = "0.0.0.0";
+const port = process.env.PORT || 10000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  res.end(`
+    <h1>Hola mundo de Eury en DevOps</h1>
+    <p>Aplicación desplegada con GitHub Actions, Docker Hub y Render.</p>
+  `);
+});
+
+server.listen(port, host, () => {
+  console.log(`Servidor corriendo en http://${host}:${port}`);
+});
